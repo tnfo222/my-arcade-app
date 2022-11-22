69aab4a3d3b82bf73f499c288f6a9f5fb4cfc789
tnfo222 <tayforbes92@hotmail.com>
2022-11-21 20:38:51 -0500

    Game page cards added and ready for game code input and routing

--- src/Components/Games.js
+++ src/Components/Games.js
@@ -1,7 +1,33 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Card from 'react-bootstrap/Card';
+import '../App.css';
 
 export default function Games () {
     return(
-        <h1> All Games </h1>
+        <div>
+            <h1> AATT Arcade </h1>
+            <div className="wrapper">
+            <Row xs={2} md={3} className="g-4">
+                {Array.from({ length: 3 }).map((_, idx) => (
+                    <Col>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src="holder.js/100px180" />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+            </div>
+        </div>
     )
 }
